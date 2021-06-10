ef6751bdb85e5d6a73a5191e6f2affa98a111986
jwelbeck52 <jwelbeck38@gmail.com>
2021-06-10 12:08:45 +0000

    refactor: move code out of window.load function; add color to operator buttons

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    
-    
-    var answer = document.getElementById('answer');
-    var inputs= document.getElementById('input-keys');
-    
-    var oneBtn = parseInt(document.getElementById('one-btn').value);
-    var twoBtn = parseInt(document.getElementById('two-btn').value);
-    // alert(oneBtn);
-    
-
-    let clearBtn = document.getElementById('clear-btn').onclick=function() {clearCalculator()};
-    var plusBtn = document.getElementById('plus-btn').onclick=function(){ addnum()};
-    function clearCalculator(){
-         
-         inputs.innerText =" ";
-         answer.innerText = 0;
-    }
-
-    function addnum(operations[]){
-        inputs.innerText = `${oneBtn} + ${twoBtn}`;
-        answer.innerText = (oneBtn + twoBtn);
-    }
-
-
-
-    
-
+    console.log('loaded');
 });
+
+
+var oneBtn = parseInt(document.getElementById('one-btn').value);
+var twoBtn = parseInt(document.getElementById('two-btn').value);
+
+var buttons = document.querySelectorAll("button");
+console.log(buttons);
+var plusBtn = document.getElementById('plus-btn');
+var answer = document.getElementById('answer');
+var inputs= document.getElementById('input-keys');
+// alert(oneBtn);
+
+
+let clearBtn = document.getElementById('clear-btn').onclick=function() {clearCalculator()};
+var plusBtn = document.getElementById('plus-btn').onclick=function(){ addnum()};
+function clearCalculator(){
+     
+     inputs.innerText =" ";
+     answer.innerText = 0;
+}
+
+function addnum(){
+    inputs.innerText = `${oneBtn} + ${twoBtn}`;
+    answer.innerText = (oneBtn + twoBtn);
+}
+
+
+function test(){
+    alert("test works");
+}
